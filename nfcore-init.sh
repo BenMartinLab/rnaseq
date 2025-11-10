18d2fb539d4cd1d07cd36ba142be24a5aec58d03
Christian Poitras <christian.poitras@ircm.qc.ca>
2025-11-10 18:49:18 -0500

    Adds script to load modules for nf-core.

---
+++ nfcore-init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ -n "$CC_CLUSTER" ]]
+then
+  module purge
+  module load StdEnv/2023
+  module load python/3.13
+  module load rust
+  module load postgresql
+  module load nextflow/25
+  module load apptainer/1
+fi
+if [[ "beluga" == "$CC_CLUSTER" ]]
+then
+  ulimit -v 40000000
+fi
+
+export SLURM_ACCOUNT=def-bmartin
+export NXF_SINGULARITY_CACHEDIR=/project/def-bmartin/NXF_SINGULARITY_CACHEDIR
+export NXF_OPTS="-Xms500M -Xmx8000M"
