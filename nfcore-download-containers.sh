1db15b5332e536c66c425477c999f60ad2ba4f7a
Christian Poitras <christian.poitras@ircm.qc.ca>
2025-11-10 18:50:03 -0500

    Adds script to download containers used by rnaseq nf-core pipeline.

---
+++ nfcore-download-containers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+script_path=$(dirname "$(readlink -f "$0")")
+cd "$script_path" || { echo "Folder $script_path does not exists"; exit 1; }
+source nfcore-init.sh
+source nfcore-env/bin/activate
+
+nf-core pipelines download \
+    --download-configuration yes \
+    --container-cache-utilisation amend \
+    --container-system singularity \
+    --compress none \
+    -d 6 \
+    -r 3.19.0 \
+    rnaseq
