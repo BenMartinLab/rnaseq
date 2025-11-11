1ae67cf842eaeadafae7d38f698c78107d721bc2
Christian Poitras <christian.poitras@ircm.qc.ca>
2025-11-10 19:03:17 -0500

    Removes useless pipeline example in nfcore-create-env.sh.

--- nfcore-create-env.sh
+++ nfcore-create-env.sh
@@ -13,11 +13,3 @@ cd "$script_path" || { echo "Folder $script_path does not exists"; exit 1; }
 python -m venv nfcore-env
 source nfcore-env/bin/activate
 python -m pip install nf_core==3.2.1
-
-echo
-echo
-
-echo "Example to run the chipseq pipeline:"
-echo "nextflow run nf-core-rnaseq_3.19.0/3_19_0/ \\"
-echo "    -profile test,alliance_canada \\"
-echo "    --outdir output"
