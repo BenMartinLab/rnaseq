a49e98a4e0ccf1b2fa76610f5bcc284625289972
Christian Poitras <christian.poitras@ircm.qc.ca>
2025-11-12 11:17:47 -0500

    Delete environment before creating it in nfcore-create-env.sh.

--- nfcore-create-env.sh
+++ nfcore-create-env.sh
@@ -10,6 +10,7 @@ fi
 script_path=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 cd "$script_path" || { echo "Folder $script_path does not exists"; exit 1; }
 
+rm -rf nfcore-env
 python -m venv nfcore-env
 source nfcore-env/bin/activate
 python -m pip install nf_core==3.2.1
