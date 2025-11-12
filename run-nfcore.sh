bf537899e61d556c7c76fc32c5447a0b15d366ce
Christian Poitras <christian.poitras@ircm.qc.ca>
2025-11-12 11:56:30 -0500

    Allows run-nfcore.sh to be submitted with sbatch.

--- run-nfcore.sh
+++ run-nfcore.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#SBATCH --account=def-bmartin
+#SBATCH --time=2-00:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+#SBATCH --output=run-nfcore-%A.out
 
 script_path=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 source "${script_path}/nfcore-modules.sh"
