7fff8a149a3a6c32da12591289c8618bf2cd25d6
Christian Poitras <christian.poitras@ircm.qc.ca>
2025-11-12 11:01:59 -0500

    Renames nfcore-init.sh to nfcore-modules.sh.

--- nfcore-download-containers.sh
+++ nfcore-download-containers.sh
@@ -2,7 +2,7 @@
 
 script_path=$(dirname "$(readlink -f "$0")")
 cd "$script_path" || { echo "Folder $script_path does not exists"; exit 1; }
-source nfcore-init.sh
+source nfcore-modules.sh
 source nfcore-env/bin/activate
 
 nf-core pipelines download \
