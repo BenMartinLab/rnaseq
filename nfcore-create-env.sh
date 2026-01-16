5246ac8e4b99a4d020683c7799539f8796aa81b1
Christian Poitras <christian.poitras@ircm.qc.ca>
2026-01-16 10:25:32 -0500

    Update nf-core tools
    Updates nf-core tools to version 3.5.1.

--- nfcore-create-env.sh
+++ nfcore-create-env.sh
@@ -14,4 +14,4 @@ rm -rf nfcore-env
 python -m venv nfcore-env
 source nfcore-env/bin/activate
 python -m pip install --upgrade pip
-python -m pip install nf_core==3.2.1 pysam==0.23.3
+python -m pip install nf_core==3.5.1 pysam==0.23.3
