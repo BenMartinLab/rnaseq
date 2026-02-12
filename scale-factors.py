3c8e5a165c1c0bdd3c286db3c925a2d3456d4178
Christian Poitras <christian.poitras@ircm.qc.ca>
2026-02-12 13:01:44 -0500

    Fix BAM file usage in scale-factors.py
    When UMI is used, --save_umi_intermeds is required to get the BAM files in the output folder. --save_umi_intermeds also output multiple additional BAM files that are not to be used with scale-factors.py.
    This fixes the issue of having multiple BAM files for each sample.

--- scale-factors.py
+++ scale-factors.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import os.path
 import re
 import statistics
 from sys import stdout, stderr
@@ -28,14 +29,14 @@ BASE_SCALE = 1000000
 def main(argv: list[str] = None):
     parser = argparse.ArgumentParser(description="Compute scale factors based on reads aligning to "
                                                  "main genome and spike-in genome.")
-    parser.add_argument("-b", "--bam", nargs="+", type=argparse.FileType('r'),
-                        help="BAM files")
-    parser.add_argument("-o", "--output", type=argparse.FileType('w'), default=stdout,
-                        help="Tab delimited file containing multiple scale factor options  (default: standard output)")
-    parser.add_argument("-s", "--spike_fasta", type=argparse.FileType('r'), default=None,
-                        help="FASTA file containing spike-in genome")
     parser.add_argument("--samplesheet", type=argparse.FileType('r'), default=None,
                         help="Sample sheet file where sample names will be used as labels")
+    parser.add_argument("-o", "--output", type=argparse.FileType('w'), default=stdout,
+                        help="Tab delimited file containing multiple scale factor options  (default: standard output)")
+    parser.add_argument("-b", "--bam", default="output/star_salmon",
+                        help="Folder where BAM files are located  (default: %(default)s)")
+    parser.add_argument("-s", "--spike_fasta", type=argparse.FileType('r'), default=None,
+                        help="FASTA file containing spike-in genome")
     parser.add_argument("-S", "--scale", type=int, default=BASE_SCALE,
                         help="Base scale to use to compute scale factors  (default: %(default)s)")
     parser.add_argument("-m", "--mean", action="store_true",
@@ -43,78 +44,48 @@ def main(argv: list[str] = None):
 
     args = parser.parse_args(argv)
 
-    labels = bam_labels(bam_files=args.bam, labels=samplesheet_to_labels(args.samplesheet))
-    scale_factors(bam_files=args.bam, output_file=args.output, spike_fasta_file=args.spike_fasta,
-                  labels=labels, scale=args.scale, mean=args.mean)
+    bam_files = get_bam_files(args.samplesheet, args.bam)
+    scale_factors(bam_files=bam_files, output_file=args.output, spike_fasta_file=args.spike_fasta,
+                  scale=args.scale, mean=args.mean)
 
 
-def samplesheet_to_labels(samplesheet_file: TextIO) -> list[str]:
+def get_bam_files(samplesheet_file: TextIO, bam_folder: str = "output/star_salmon") -> dict[str, str]:
     """
-    Returns a list of BAM file labels based on sample sheet file.
+    Finds BAM file associated with each sample.
 
     :param samplesheet_file: sample sheet file
-    :return: list of BAM file labels based on sample sheet file
+    :param bam_folder: folder where BAM files are located
+    :return: list of BAM file associated with each sample
     """
-    labels = []
-    if samplesheet_file.name.endswith(".csv"):
-        headers = samplesheet_file.readline().rstrip("\r\n").split(",")
-        headers = [header.strip("\"") for header in headers]
-        replicate_index = headers.index("replicate") if "replicate" in headers else 0
-        for line in samplesheet_file:
-            columns = line.rstrip("\r\n").split(",")
-            columns = [column.strip("\"") for column in columns]
-            label = columns[0]
-            if replicate_index:
-                # Assume chipseq nf-core pipeline.
-                label=f"{label}_REP{columns[replicate_index]}"
-            labels.append(label)
-    elif samplesheet_file.name.endswith(".txt") or samplesheet_file.name.endswith(".tsv"):
-        for line in samplesheet_file:
-            if line.startswith("#"):
-                continue # Skip header.
-            columns = line.rstrip("\r\n").split("\t")
-            labels.append(columns[0])
-    else:
-        print(f"Warning: sample sheet file format {samplesheet_file.name} not supported", file=stderr)
-    return labels
+    bam_files = {}
+    headers = samplesheet_file.readline().rstrip("\r\n").split(",")
+    headers = [header.strip("\"") for header in headers]
+    replicate_index = headers.index("replicate") if "replicate" in headers else 0
+    for line in samplesheet_file:
+        columns = line.rstrip("\r\n").split(",")
+        columns = [column.strip("\"") for column in columns]
+        sample = columns[0]
+        if replicate_index:
+            # Assume chipseq nf-core pipeline.
+            sample=f"{sample}_REP{columns[replicate_index]}"
+        bam_file = os.path.join(bam_folder, sample + ".umi_dedup.sorted.bam")
+        if not os.path.exists(bam_file):
+            bam_file = os.path.join(bam_folder, sample + ".markdup.sorted.bam")
+        if not os.path.exists(bam_file):
+            print(f"Error: no BAM file could be found in folder {bam_folder} for sample {sample}", file=stderr)
+            exit(1)
+        bam_files[sample] = bam_file
+    return bam_files
 
 
-def bam_labels(bam_files: list[TextIO], labels: list[str]) -> dict[str, str]:
-    """
-    Finds label for each BAM file.
-
-    :param bam_files: BAM files
-    :param labels: Labels to use instead of BAM filename in BAM output column
-    :return: Dictionary of labels mapped by BAM filename
-    """
-    bam_filenames = [bam_file.name for bam_file in bam_files]
-
-    labels_by_filename = {}
-    for label in labels:
-        bam_filenames_with_label = [bam_filename for bam_filename in bam_filenames if label in bam_filename]
-        if not bam_filenames_with_label:
-            print(f"Warning: label {label} not found in any BAM filenames", file=stderr)
-        elif len(bam_filenames_with_label) == 1:
-            labels_by_filename[bam_filenames_with_label[0]] = label
-        else:
-            print(f"Warning: label {label} found multiple BAM filenames ({bam_filenames_with_label})", file=stderr)
-            for filename in bam_filenames_with_label:
-                labels_by_filename[filename] = label
-    for bam_filename in bam_filenames:
-        if bam_filename not in labels_by_filename:
-            print(f"Warning: BAM file {bam_filename} does not have a label", file=stderr)
-    return labels_by_filename
-
-
-def scale_factors(bam_files: list[TextIO], output_file: TextIO, spike_fasta_file: TextIO, labels: dict[str, str],
+def scale_factors(bam_files: dict[str, str], output_file: TextIO, spike_fasta_file: TextIO,
     scale: int = BASE_SCALE, mean: bool = False):
     """
     Compute scale factors based on reads aligning to main genome and spike-in genome.
 
-    :param bam_files: BAM files
+    :param bam_files: BAM files associated with each sample
     :param output_file: Tab delimited file containing multiple scale factor options
     :param spike_fasta_file: FASTA file containing spike-in genome
-    :param labels: Labels to use instead of BAM filename in BAM output column
     :param scale: Base scale to use to compute scale factors
     :param mean: Divide scale factors by the mean of all scale factors
     """
@@ -129,8 +100,8 @@ def scale_factors(bam_files: list[TextIO], output_file: TextIO, spike_fasta_file
     output_file.write("\n")
 
     bam_scales = {}
-    for i in range(0, len(bam_files)):
-        bam_file = bam_files[i]
+    for sample in bam_files:
+        bam_file = bam_files[sample]
         align = pysam.AlignmentFile(bam_file, "rb")
         read_filter = lambda read : (((read.is_paired and read.is_proper_pair) or not read.is_paired)
                                      and not read.is_qcfail and not read.is_secondary and not read.is_supplementary)
@@ -140,13 +111,12 @@ def scale_factors(bam_files: list[TextIO], output_file: TextIO, spike_fasta_file
             try:
                 spike_read_count += align.count(contig=chromosome, read_callback=read_filter)
             except ValueError:
-                print(f"Warning: BAM file {bam_file.name} does not contain chromosome {chromosome} "
+                print(f"Warning: BAM file {bam_file} does not contain chromosome {chromosome} "
                       f"from spike-in FASTA", file=stderr)
         read_count -= spike_read_count
-        label = labels[bam_file.name] if bam_file.name in labels else bam_file.name
-        bam_scales[bam_file.name] = BamScale(label, read_count, scale/read_count if read_count else None)
+        bam_scales[sample] = BamScale(sample, read_count, scale/read_count if read_count else None)
         if spike_fasta_file:
-            bam_scales[bam_file.name].set_spike_scale(spike_read_count,
+            bam_scales[sample].set_spike_scale(spike_read_count,
                                                       scale/spike_read_count if spike_read_count else None)
     scale_factor_mean = statistics.mean(
         [bam_scale.scale_factor for bam_scale in bam_scales.values() if bam_scale.scale_factor])
